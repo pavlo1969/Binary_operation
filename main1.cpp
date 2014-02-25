abc791815754067cf0f1edce9ea72143dfbbecb8
Pavlo <paulshynal@gmail.com>
2014-02-25 20:25:39 +0200

    Код спп

---
+++ main1.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,sum=0;
+    for(i=0,i<=10,i++)
+    sum+=i;
+    cout<<sum<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
